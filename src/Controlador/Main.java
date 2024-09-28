7a39e4251a99e17fd704da2f94be22e82cc7b311
Vale <Vale@Vale>
2024-09-27 21:02:55 -0500

    intento2

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -5,5 +5,6 @@ import VentanaVista.VentanaVistaInicio;
 public class Main {
     public static void main(String[] args) {
         new VentanaVistaInicio().setVisible(true);
+        //tmabooada
     }
 }
